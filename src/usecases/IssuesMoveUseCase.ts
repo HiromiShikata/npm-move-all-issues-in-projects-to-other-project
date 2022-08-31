909bb52e9f22061a5aa560881c017152a57def43
HiromiShikata <shikata.hiromi@gmail.com>
2022-08-31 23:05:59 +0000

    chore: return undefined if repository is not found

--- src/usecases/IssuesMoveUseCase.ts
+++ src/usecases/IssuesMoveUseCase.ts
@@ -10,8 +10,16 @@ export class IssuesMoveUseCase {
 
     execute = async (fromName: string, toName: string) => {
         const from = await this.projectRepository.get(fromName)
+        if (!from) throw new ProjectNotFoundError(fromName)
         const to = await this.projectRepository.get(toName)
+        if (!to) throw new ProjectNotFoundError(toName)
         const issues = await this.issueRepository.get(from)
-        await this.issueRepository.bulkUpdateProject(issues, to);
+        await this.issueRepository.bulkUpdateProject(issues, to)
+    }
+}
+
+export class ProjectNotFoundError extends Error {
+    constructor(projectName: string) {
+        super(`${projectName} is not found.`)
     }
 }
