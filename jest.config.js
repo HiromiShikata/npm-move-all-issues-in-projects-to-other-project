65d9a02d4b8a6ed6aefb9fb031206765a7f161b1
HiromiShikata <shikata.hiromi@gmail.com>
2022-08-31 22:57:57 +0000

    chore(ci): check test coverage by each layers

--- jest.config.js
+++ jest.config.js
@@ -7,9 +7,18 @@ module.exports = {
     transformIgnorePatterns: ['<rootDir>/node_modules/'],
     collectCoverage: true,
     coverageDirectory: "reports/coverage",
+    collectCoverageFrom: ['src/**/*'],
     reporters: [
         "default",
         ["jest-junit", {"outputDirectory": "reports/jest-junit"}],
         ["./node_modules/jest-html-reporter", {"outputPath": "reports/jest-html-reporter/index.html"}]
-    ]
+    ],
+    coverageThreshold: {
+        global: {
+            branches: 100,
+            functions: 100,
+            lines: 100,
+            statements: 0
+        }
+    }
 };
