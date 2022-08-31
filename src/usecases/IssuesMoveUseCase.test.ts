4a85c66ccfda4078f330b0b37a6af7f0e001bd47
HiromiShikata <shikata.hiromi@gmail.com>
2022-08-31 23:36:25 +0000

    test: add test for error case in IssueMoveUseCase

--- src/usecases/IssuesMoveUseCase.test.ts
+++ src/usecases/IssuesMoveUseCase.test.ts
@@ -1,27 +1,66 @@
-import { IssuesMoveUseCase } from './IssuesMoveUseCase';
+import { IssuesMoveUseCase, ProjectNotFoundError } from './IssuesMoveUseCase';
 import Project from '../models/Project';
 import Issue from '../models/Issue';
 
 describe('IssuesMoveUseCase', () => {
   describe('execute', () => {
+    const issueRepository = {
+      get: jest.fn(async (project: Project): Promise<Issue[]> => {
+        return [];
+      }),
+      bulkUpdateProject: jest.fn(
+        async (issues: Issue[], project: Project): Promise<void> => {},
+      ),
+    };
+    const projectRepository = {
+      get: jest.fn(async (name: string): Promise<Project | undefined> => {
+        return new Project();
+      }),
+    };
+    beforeEach(() => {
+      jest.clearAllMocks();
+    });
+
     test('success', async () => {
-      const issueRepository = {
-        get: jest.fn(async (project: Project): Promise<Issue[]> => {
-          return [];
-        }),
-        bulkUpdateProject: jest.fn(
-          async (issues: Issue[], project: Project): Promise<void> => {},
-        ),
-      };
-      const projectRepository = {
-        get: jest.fn(async (name: string): Promise<Project> => {
-          return new Project();
-        }),
-      };
       const useCase = new IssuesMoveUseCase(projectRepository, issueRepository);
       await useCase.execute('from', 'to');
       expect(issueRepository.get.mock.calls.length).toEqual(1);
       expect(issueRepository.bulkUpdateProject.mock.calls.length).toEqual(1);
+      expect(projectRepository.get.mock.calls.length).toEqual(2);
+    });
+    test('failed with NotFoundError for from', async () => {
+      projectRepository.get = jest.fn(async (name: string) =>
+        Promise.resolve(undefined),
+      );
+      const useCase = new IssuesMoveUseCase(projectRepository, issueRepository);
+
+      try {
+        await useCase.execute('from', 'to');
+        fail();
+      } catch (ex) {
+        expect(ex).toBeInstanceOf(ProjectNotFoundError);
+        expect((ex as Error).message).toEqual('from is not found.');
+      }
+      expect(issueRepository.get.mock.calls.length).toEqual(0);
+      expect(issueRepository.bulkUpdateProject.mock.calls.length).toEqual(0);
+      expect(projectRepository.get.mock.calls.length).toEqual(1);
+    });
+
+    test('failed with NotFoundError for to', async () => {
+      projectRepository.get = jest.fn(async (name: string) =>
+        Promise.resolve(name === 'from' ? new Project() : undefined),
+      );
+      const useCase = new IssuesMoveUseCase(projectRepository, issueRepository);
+
+      try {
+        await useCase.execute('from', 'to');
+        fail();
+      } catch (ex) {
+        expect(ex).toBeInstanceOf(ProjectNotFoundError);
+        expect((ex as Error).message).toEqual('to is not found.');
+      }
+      expect(issueRepository.get.mock.calls.length).toEqual(0);
+      expect(issueRepository.bulkUpdateProject.mock.calls.length).toEqual(0);
       expect(projectRepository.get.mock.calls.length).toEqual(2);
     });
   });
