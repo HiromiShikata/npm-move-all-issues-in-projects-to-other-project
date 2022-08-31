21ebf829dbc3c34ddd342754d1e571430238ab97
HiromiShikata <shikata.hiromi@gmail.com>
2022-08-31 23:05:59 +0000

    chore: return undefined if repository is not found

--- src/usecases/repositories/ProjectRepository.ts
+++ src/usecases/repositories/ProjectRepository.ts
@@ -1,6 +1,5 @@
 import Project from "../../models/Project";
 
-export default interface ProjectRepository{
-    get(name:string): Promise<Project>
-
+export default interface ProjectRepository {
+    get(name: string): Promise<Project | undefined>
 }
